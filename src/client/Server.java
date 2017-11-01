bc93d383dd25d0a4362c3d34b604c88c4440cbb6
SHADOW <ucshadow@gmail.com>
2017-11-01 12:00:03 +0100

    added client side from best group

---
+++ src/client/Server.java
@@ -0,0 +1,41 @@
+package client;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private final int PORT = 6789;
+    public static ArrayList<Runnable> clientList;
+
+    public Server() {
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        clientList = new ArrayList<>();
+    }
+
+    public void startServer() {
+        while (true) {
+            try {
+                Connection newClient = new Connection(serverSocket.accept());
+                clientList.add(newClient);
+                new Thread(newClient).start();
+                System.out.println("client.Client connected");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Server().startServer();
+    }
+
+
+}
